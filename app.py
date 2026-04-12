d80627f56549450d267916eeef8b236b3aefb748
David Rubin <idf775@gmail.com>
2026-04-12 14:24:22 +0300

    add gitignore and update app.py

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
-from flask import Flask
-app = Flask(__name__)
+import requests
+
+# 1. הכתובת של ה-API (תחנת החלל)
+url = "http://api.open-notify.org/iss-now.json"
+
+# 2. שליחת הבקשה
+response = requests.get(url)
+
+# 3. בדיקה אם קיבלנו 200 (כמו שהמרצה אמר)
+if response.status_code == 200:
+    data = response.json() # הופך את הטקסט למילון של פייתון
+    print("Success!")
+    print(data)
+else:
+    print(f"Error: {response.status_code}")
+    print(f"The type is: {type(response)}")
+    print(f"The response object is: {response}")
