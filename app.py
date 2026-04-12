efd0e500d605bd834d1e759b4f09ed37df82c65f
David Rubin <idf775@gmail.com>
2026-04-12 14:46:16 +0300

    tested jsonplaceholder cause nasa api was dead lol

--- app.py
+++ app.py
@@ -1,25 +1,10 @@
-```
 import requests
 from datetime import datetime
 
-response = requests.get("http://api.open-notify.org/iss-now.json")
-
-# class MyClass:
-#     def __init__(self, name):
-#         self.name = name
-
-#     def greet(self):
-#         return f"Hello, {self.name}!"
-
+response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
 
 data = response.json()
-# print(type(response))
-# print(response)
-# print(data)
 
-print(data["iss_position"])
-ts = data["timestamp"]
-TIME = datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-
-print(f"Current ISS location at {TIME} is LAT: {data["iss_position"]["latitude"]}, LON: {data["iss_position"]["longitude"]} ")
-```
+print(f"Post ID: {data['id']}")
+print(f"Title: {data['title']}")
+print(f"Body: {data['body']}")
