5bf296b377fc3e9fae9051c5233affee8bcd5b62
David Rubin <idf775@gmail.com>
2026-04-15 19:53:11 +0300

    code 400

--- errors.py
+++ errors.py
@@ -9,3 +9,17 @@ def handle_not_found(e):
     return jsonify({
         "error": str(e)
     }), 404
+
+# 400 בעיה בבקשה של קוד 400ה
+@errors_bp.app_errorhandler(BadRequest)
+def handle_bad_request(e):
+    return jsonify({
+        "error": str(e)
+    }), 400
+
+# 422
+@errors_bp.app_errorhandler(UnprocessableEntity)
+def handle_unprocessable(e):
+    return jsonify({
+        "error": str(e)
+    }), 422
