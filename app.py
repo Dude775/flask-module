67cd2a82ff77e173a3b2e76f28cc30264e4c11cc
David Rubin <idf775@gmail.com>
2026-04-18 17:13:45 +0300

    update blueprint name -> todos_bp

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from flask import Flask
-from routes import tasks_bp
+from routes import todos_bp
 from errors import errors_bp
-
 
 app = Flask(__name__)
 
 # חיבור כל ה-blueprints
-app.register_blueprint(tasks_bp)
+app.register_blueprint(todos_bp)
 app.register_blueprint(errors_bp)
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
